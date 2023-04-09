3beca97001616a6538ad9799a683bfd81ee2fb2a
Hans Bhatia <h23bhati@uwaterloo.ca>
2023-04-09 01:59:59 -0400

    feat(browser-extension): add AccountApi object
    Added working signing with dynamically checked address type
    
    test(browser-extension): added simple error throwing test for accountApi

--- apps/browser-extension/services/AccountApi.ts
+++ apps/browser-extension/services/AccountApi.ts
@@ -1,10 +1,37 @@
-// export interface BaseApiParams {
-//   provider: Provider
-//   entryPointAddress: string
-//   accountAddress?: string
-// }
+// Account API
+// used to invoke sign transactions?
+// requires: sign function with eoa
+// get scw Address?
+// requires: chain
+// estimate Gas: will be done by gg
 
-// export class AccountApi {
-//     // infer chain from provider
+import { getTransactionHash } from "./signingUtils";
+import type { Transaction, signFunction } from "./types";
+import { Address } from "./zodSchema";
 
-// }
+export interface AccountAPIParams {
+  getSignature: signFunction;
+  eoa: string;
+  chainId: number;
+}
+
+export class AccountAPI {
+  getSignature: signFunction;
+  chainId: number;
+  eoa: string;
+
+  constructor(params: AccountAPIParams) {
+    const parsed = Address.safeParse(params.eoa);
+    if (!parsed.success) throw new Error("Invalid EOA");
+    this.getSignature = params.getSignature;
+    this.chainId = params.chainId;
+    this.eoa = params.eoa;
+  }
+
+  async createSignedTransaction(transaction: Transaction): Promise<string> {
+    const txnHash = getTransactionHash(transaction, this.chainId);
+    const signedTxn = await this.getSignature(txnHash);
+
+    return signedTxn;
+  }
+}
---
+++ apps/browser-extension/test/AccountApi.test.ts
@@ -0,0 +1,29 @@
+import { describe, test } from "@jest/globals";
+import { type Bytes, ethers } from "ethers";
+import type { signFunction } from "services/types";
+
+import { AccountAPI, type AccountAPIParams } from "../services/AccountApi";
+
+describe("AccountApi Testing", () => {
+  const wallet = ethers.Wallet.createRandom();
+  let sigFunction: signFunction;
+  let params: AccountAPIParams;
+  beforeEach(() => {
+    sigFunction = (message: Bytes): Promise<string> => {
+      return wallet.signMessage(message);
+    };
+    params = {
+      getSignature: sigFunction,
+      eoa: wallet.address,
+      chainId: 1, // mainnet
+    };
+  });
+  test("Invalid EOA", () => {
+    const badEOAParams: AccountAPIParams = {
+      getSignature: sigFunction,
+      eoa: "Invalid Address",
+      chainId: 1, // mainnet
+    };
+    expect(() => new AccountAPI(badEOAParams)).toThrow("Invalid EOA");
+  });
+});
