7e306b9a9c19bc9e7a66f89f8b0fef7a0c13fa3b
Hans Bhatia <h23bhati@uwaterloo.ca>
2023-04-08 20:44:21 -0400

    feat(browser-extension): added ts-jest feat(browser-extension): add signing utils test(browser-extension): add signing utils tests

--- apps/browser-extension/services/signingUtils.ts
+++ apps/browser-extension/services/signingUtils.ts
@@ -1,84 +1,72 @@
 import { ethers } from "ethers";
 
-interface Transaction {
-  gasLimit: number;
-  value: number;
-  nonce: number;
-  target: string;
-  revertOnError: boolean;
-  data: string;
+// Define an interface for Ethereum transactions
+export interface Transaction {
+  gasLimit: ethers.BigNumber; // The maximum amount of gas that can be used for the transaction
+  value: ethers.BigNumber; // The amount of Ether to be transferred with the transaction
+  nonce: ethers.BigNumber; // A unique identifier for the sender's account
+  target: string; // The address of the contract or account that will receive the transaction
+  revertOnError: boolean; // Whether the transaction should revert if an error occurs
+  data: string; // The data to be sent with the transaction
 }
 
+// Define the EIP-712 signature and type hash
 const EIP_712_SIG = "EIP712Domain(string name,uint256 chainId)";
-
-const EIP_712_TYPE_HASH: string = ethers.utils.keccak256(
+export const EIP_712_TYPE_HASH: string = ethers.utils.keccak256(
   ethers.utils.toUtf8Bytes(EIP_712_SIG),
 );
 
-const EIP_712_DOMAIN_HASH: string = ethers.utils.keccak256(
+// Define the domain hash
+export const EIP_712_DOMAIN_HASH: string = ethers.utils.keccak256(
   ethers.utils.toUtf8Bytes("GASLESS_GRAPES_WALLET"),
 );
 
-function getTransactionHash(transaction: Transaction, chainId: number): string {
-  const domainSeparator: string = ethers.utils.keccak256(
+// Get the domain separator for the given chain ID
+export const getDomainSeparator = (chainId: number): string => {
+  return ethers.utils.keccak256(
     ethers.utils.defaultAbiCoder.encode(
-      ["bytes32", "string", "uint256"] as string[],
+      ["bytes32", "bytes32", "uint256"] as string[],
       [EIP_712_TYPE_HASH, EIP_712_DOMAIN_HASH, chainId],
     ),
   );
-
-  //   const message = ethers.utils.keccak256(
-  //     ethers.utils.solidityPack(
-  //       ["bytes32", "bytes32"],
-  //       [
-  //         ethers.utils.keccak256(
-  //           ethers.utils.toUtf8Bytes(
-  //             "Transaction(uint256 gasLimit,uint256 value,uint256 nonce,address target,bool revertOnError,bytes data)",
-  //           ),
-  //         ),
-  //         ethers.utils.keccak256(
-  //           ethers.utils.defaultAbiCoder.encode(
-  //             [
-  //               "uint256",
-  //               "uint256",
-  //               "uint256",
-  //               "address",
-  //               "bool",
-  //               "bytes",
-  //             ] as string[],
-  //             [
-  //               transaction.gasLimit,
-  //               transaction.value,
-  //               transaction.nonce,
-  //               transaction.target,
-  //               transaction.revertOnError,
-  //               transaction.data,
-  //             ] as string[],
-  //           ),
-  //         ),
-  //       ],
-  //     ),
-  //   );
-
-  //   const hashedMessage: string = ethers.utils.keccak256(
-  //     ethers.utils.solidityPack(
-  //       ["bytes1", "bytes1", "bytes32", "bytes32"] as string[],
-  //       ["0x19", "0x01", domainSeparator, message] as string[],
-  //     ),
-  //   );
-
-  //return hashedMessage;
-  return domainSeparator;
-}
-
-const txn: Transaction = {
-  gasLimit: 2,
-  value: 0,
-  nonce: 1,
-  target: "0x61c66721D9094DA3ceCED0F2C52c36c3AE94A319",
-  revertOnError: true,
-  data: "",
 };
-const hash = getTransactionHash(txn, 1);
 
-console.log(hash);
+// Get the hash of the given transaction
+export const getTxnHash = (transaction: Transaction): string => {
+  return ethers.utils.keccak256(
+    ethers.utils.defaultAbiCoder.encode(
+      ["uint256", "uint256", "address", "bool", "uint256", "bytes"] as string[],
+      [
+        transaction.nonce,
+        transaction.gasLimit,
+        transaction.target,
+        transaction.revertOnError,
+        transaction.value,
+        transaction.data,
+      ] as string[],
+    ),
+  );
+};
+
+// Get the hash of the signed message for the given transaction and chain ID
+export function getTransactionHash(
+  transaction: Transaction,
+  chainId: number,
+): string {
+  // Get the domain separator
+  const domainSeparator: string = getDomainSeparator(chainId);
+
+  // Construct the message to sign
+  const message = ethers.utils.solidityPack(
+    ["bytes2", "bytes32", "bytes32"] as string[],
+    [
+      ethers.utils.toUtf8Bytes("\x19\x01"),
+      domainSeparator,
+      getTxnHash(transaction),
+    ] as string[],
+  );
+
+  // Get the hash of the signed message
+  const hashedMessage: string = ethers.utils.keccak256(message);
+  return hashedMessage;
+}
---
+++ apps/browser-extension/test/signingUtils.test.ts
@@ -0,0 +1,49 @@
+import { describe, test } from "@jest/globals";
+import { ethers } from "ethers";
+
+import {
+  EIP_712_DOMAIN_HASH,
+  EIP_712_TYPE_HASH,
+  type Transaction,
+  getDomainSeparator,
+  getTransactionHash,
+  getTxnHash,
+} from "../services/signingUtils";
+
+describe("signing tests", () => {
+  test("test typehash", () => {
+    expect(EIP_712_TYPE_HASH).toBe(
+      "0xcc85e4a69ca54da41cc4383bb845cbd1e15ef8a13557a6bed09b8bea2a0d92ff",
+    );
+  }),
+    test("test domainhash", () => {
+      expect(EIP_712_DOMAIN_HASH).toBe(
+        "0x3c924ac159c18b9abc95e27241d8d48f7303b8371d5de879bcdbc55a381559eb",
+      );
+    }),
+    test("test domainseparator", () => {
+      // hardcoded chain one, crosschecked with remix
+      expect(getDomainSeparator(1)).toBe(
+        "0x09f0d3d6d21027515bd4d9c8f9407fd674f4924fe5485ce3fe25cb2ef1671246",
+      );
+    }),
+    test("basic hash", () => {
+      const ABI = ["function increment() view"];
+      const iface = new ethers.utils.Interface(ABI);
+      const txn: Transaction = {
+        gasLimit: ethers.BigNumber.from(1000000),
+        value: ethers.BigNumber.from(0),
+        nonce: ethers.BigNumber.from(12),
+        target: "0x5253F42a13f14a50E8783b23a787247002e7a9eC",
+        revertOnError: false,
+        data: iface.encodeFunctionData("increment"),
+      };
+      expect(getTxnHash(txn)).toBe(
+        "0x7cddc12e3f1c40e3d4e7a35c3966403d2cf04ff462c29ef0259598ce96c5c15a",
+      );
+
+      expect(getTransactionHash(txn, 1)).toBe(
+        "0x304ed2d2fbd66467f1d79e7b83b274dda80cb8fe92002b918b83c574888ddea3",
+      );
+    });
+});
